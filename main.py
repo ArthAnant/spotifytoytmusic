1534ccd73132adff726f76e89863b3a45d7b303d
ArthAnant <arthanant47@gmail.com>
2025-03-31 02:48:21 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+load_dotenv()
+
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
+    client_id=os.getenv("SPOTIFY_CLIENT_ID"),
+    client_secret=os.getenv("SPOTIFY_CLIENT_SECRET")
+))
