69c8c680754e63ba5368deff114c791e3443baca
ArthAnant <arthanant47@gmail.com>
2025-04-08 11:58:55 +0100

    Working migration for test playlist

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-from ytmusicapi import YTMusic
+from ytmusicapi import YTMusic, OAuthCredentials
 
 load_dotenv()
 
@@ -30,8 +30,37 @@ def get_spotify_playlist_tracks(playlist_url):
     
     return tracks
 
+def create_youtube_music_playlist(tracks, playlist_name):
+    ytmusic = YTMusic('oauth.json', oauth_credentials=OAuthCredentials(client_id=os.getenv("YT_CLIENT_ID"), client_secret=os.getenv("YT_CLIENT_SECRET")))
+    # Create playlist
+    playlist_id = ytmusic.create_playlist(
+        title=playlist_name,
+        description="Migrated from Spotify"
+    )
+    print(tracks)
+    for track in tracks:
+        query_song = f"{track['title']} {track['artist']}"
+        print(f"Searching for: {query_song}")
+        search = ytmusic.search(query_song, limit=1)
+        if search:
+            video_id = search[0]['videoId']
+            ytmusic.add_playlist_items(
+                playlistId=playlist_id,
+                videoIds=[video_id]
+            )
+            print(f"Added: {query_song}")
+        else:
+            print(f"Not found: {query_song}")
+    
+    return playlist_id
 if __name__ == "__main__":
     spotify_url = input("Enter Spotify Playlist URL: ")
-    print("\nFetching Spotify playlist...")
+    playlist_name = input("Enter New Playlist Name: ")
     tracks = get_spotify_playlist_tracks(spotify_url)
     print(f"Found {len(tracks)} tracks")
+    
+    yt_playlist_id = create_youtube_music_playlist(tracks, playlist_name)
+    print(f"Created YouTube Music playlist: https://music.youtube.com/playlist?list={yt_playlist_id}")
+    # print("\nFetching Spotify playlist...")
+    # tracks = get_spotify_playlist_tracks(spotify_url)
+    # print(f"Found {len(tracks)} tracks")
